4464e4d90cc0b180117976316d3dc5d225ecdb0c
dbonafin <demianbonafina@gmail.com>
2022-07-08 15:01:40 +0200

    reset done, bootstrap added

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,26 @@
+<template>
+
+    <div class="reset-test">
+        <h1>Reset effettuato</h1>
+    </div>
+
+</template>
+
+<script>
+
+    export default {
+        name: "PageHeader"
+    }
+
+</script>
+
+<style lang="scss">
+
+    .reset-test {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        text-align: center;
+        color: blueviolet;
+        margin-top: 60px;
+    }
+    
+</style>
